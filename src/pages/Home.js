1484df5f768a2d9a2a6474b07446d02e776f2d6d
gabriel427131 <gabriel.d.oliveira1@hotmail.com>
2023-10-24 14:41:46 -0600

    Add react router
    - Implement App.js with path to different "pages"
    
    - Add Home and Products components

---
+++ src/pages/Home.js
@@ -0,0 +1,5 @@
+const HomePage = () => {
+  return <h1>React Router Home Page Test</h1>;
+};
+
+export default HomePage;
---
+++ src/pages/Products.js
@@ -0,0 +1,5 @@
+const ProductsPage = () => {
+  return <h1>React Router PRODUCTS PAGE Test</h1>;
+};
+
+export default ProductsPage;
