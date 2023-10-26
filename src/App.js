7f8380d982cbdc4a953067208bafe945130a29e2
gabriel427131 <gabriel.d.oliveira1@hotmail.com>
2023-10-26 13:05:12 -0600

    Update products and page error
    - Implement ErrorPage into app.js for wrong url error page fallback
    
    - Implement NavLink into MainNavigation.js to highlight in which page is selected
    
    - Add Error.js into pages and build it with respective data
    
    - Add in Home.js a programmatically imperative way to change pages into a button
    
    - Add ProductDetail.js and import useParams to later dynamically fetch database data of detailed products
    
    - Update Products.js and Root.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import {
   // createRoutesFromElements,
   RouterProvider,
 } from "react-router-dom";
+
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
 import RootLayout from "./pages/Root";
+import ErrorPage from "./pages/Error";
+import ProductDetailPage from "./pages/ProductDetail";
 
 // const routeDefinitions = createRoutesFromElements(
 //   <Route>
@@ -20,9 +23,11 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
       { path: "/", element: <HomePage /> },
       { path: "/products", element: <ProductsPage /> },
+      { path: "/products/:productId", element: <ProductDetailPage /> },
     ],
   },
 ]);
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,5 +1,14 @@
 const ProductsPage = () => {
-  return <h1>React Router PRODUCTS PAGE Test</h1>;
+  return (
+    <>
+      <h1>React Router PRODUCTS PAGE Test</h1>;
+      <ul>
+        <li>Product 1</li>
+        <li>Product 2</li>
+        <li>Product 3</li>
+      </ul>
+    </>
+  );
 };
 
 export default ProductsPage;
